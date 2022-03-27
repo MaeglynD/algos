bc18e25bf901453894187cb0d481b0adc6428405
MaeglynD <maeglyn.davies@gmail.com>
2022-03-27 23:58:59 +0100

    changed indentation

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -2,44 +2,44 @@
 using namespace std;
 
 int partition(vector<int>& nums, int l, int r){
-	int q = l-1;
-	
-	for (int i = l; i < r; ++i) {
-		if (nums[i] <= nums[r]) {
-			q++;
-		  swap(nums[q], nums[i]);
-		}
-	}
+  int q = l-1;
 
-	q++;
-	swap(nums[q], nums[r]);
+  for (int i = l; i < r; ++i) {
+    if (nums[i] <= nums[r]) {
+      q++;
+      swap(nums[q], nums[i]);
+    }
+  }
 
-	return q;
+  q++;
+  swap(nums[q], nums[r]);
+
+  return q;
 }
 
 void quickSort(vector<int>& nums, int l, int r){
-	if (l < r) {
-		int p = partition(nums, l, r);
+  if (l < r) {
+    int p = partition(nums, l, r);
 
-		quickSort(nums, l, p-1);
-		quickSort(nums, p+1, r);
-	}
+    quickSort(nums, l, p-1);
+    quickSort(nums, p+1, r);
+  }
 }
 
 int main() {
-	vector<vector<int>>	testCases = {
-		{10, 80, 30, 90, 40, 50, 70},
-		{9, 7, 5, 11, 12, 2, 14, 3, 10, 4, 6}
-	};
+  vector<vector<int>>	testCases = {
+    {10, 80, 30, 90, 40, 50, 70},
+    {9, 7, 5, 11, 12, 2, 14, 3, 10, 4, 6}
+  };
 
-	for (auto& test : testCases) {
-		quickSort(test, 0, test.size()-1);
+  for (auto& test : testCases) {
+    quickSort(test, 0, test.size()-1);
 
-		for (int& x : test) {
-			cout << x << ", ";
-		}
-		cout << endl;
-	}
+    for (int& x : test) {
+     cout << x << ", ";
+    }
+    cout << endl;
+  }
 
 	return 0;
 }
